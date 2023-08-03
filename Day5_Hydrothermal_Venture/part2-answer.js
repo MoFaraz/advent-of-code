525868ce4e07608b4a5f0537690611dbb3962db0
mofaraz <mofaraz@gmail.com>
2023-08-03 17:43:24 +0330

    find answer of part2

---
+++ Day5_Hydrothermal_Venture/part2-answer.js
@@ -0,0 +1,74 @@
+const fs = require('fs')
+
+const map = {}
+const addToMap = (map, x, y) => {
+    const key = `${x},${y}`
+    if (map[key]) 
+        map[key] ++
+    else 
+        map[key] = 1
+}
+
+const countOverlap = () => {
+    const points = Object.keys(map)
+    return points.reduce((total, point) => {
+        if (map[point] > 1) {
+            return total + 1
+        }
+        return total
+    }, 0)
+}
+
+const range = (num1, num2) => {
+    const count = Math.abs(num2 - num1) + 1 
+    return ([...Array(count).keys()].map((p) => {
+        if (num1 > num2)
+            return num1 - p
+        else 
+            return num1 + p
+    }))
+    
+}
+
+fs.readFile('inputs.txt', (err, data) => {
+    if (err) console.log(err)
+    
+    const lines = data.toString().split('\n').map((line) => line.replaceAll(' -> ', ',').split(',').map((n) => Number(n)))
+   
+    lines.forEach((line) => {
+        const [x1,y1,x2,y2] = line;
+        const isVertical = x1 === x2
+        const isHorizantal = y1 === y2
+        const isDiagnol = !isVertical && !isHorizantal
+        
+
+        if (isVertical) {
+            const minY = Math.min(y1, y2)
+            const maxY = Math.max(y1, y2)
+
+            for (let i = minY; i <= maxY; i++) {
+                addToMap(map, x1, i)
+            }
+        }
+        if (isHorizantal) {
+            const minX = Math.min(x1, x2)
+            const maxX = Math.max(x1, x2)
+
+            for (let i = minX; i <= maxX; i++){
+                addToMap(map, i, y1)
+            }
+        }
+
+        if (isDiagnol) {
+            const rangeX = range(x1, x2)
+            const rangeY = range(y1, y2)
+            rangeX.forEach((x, index) => {
+                const y = rangeY[index]
+                addToMap(map, x, y)
+            })
+            
+        }
+    })
+    
+        console.log(countOverlap())
+})
