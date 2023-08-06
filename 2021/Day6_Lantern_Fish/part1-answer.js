ac7977f5bb4d6c374aebb6ea930a6d73aec8d12e
mofaraz <mofaraz@gmail.com>
2023-08-06 10:47:02 +0330

    find answer of part1

---
+++ 2021/Day6_Lantern_Fish/part1-answer.js
@@ -0,0 +1,30 @@
+const {readInput} = require('../utils/readInput')
+
+const solve = (data) => {
+    let fish = data.split(',').map((n) => Number(n))
+
+    const getNewFishPopulation = (fish) => {
+    let newFishCount = 0
+    const newFishPopulation = fish.map((f) => {
+        if (f === 0) {
+        newFishCount++
+        return 6
+    }
+        return f - 1
+  })
+    newFishPopulation.push(...Array(newFishCount).fill(8))
+    return newFishPopulation
+}
+    for (let day = 1; day <= 80; day++) {
+        fish = getNewFishPopulation(fish)
+    }
+    return fish.length
+
+}
+
+
+(async () => {
+    const data = await readInput('./inputs.txt')
+    console.log(solve(data))
+    })()
+
