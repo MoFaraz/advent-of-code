e55b82c672d787d3938a14c5a03979c2793eb1ed
mofaraz <mofaraz@gmail.com>
2023-08-21 11:50:03 +0330

    solve part2 of day10

---
+++ 2021/Day10_Syntax_Scoring/part2-answer.js
@@ -0,0 +1,68 @@
+const { readInput } = require('../utils/readInput')
+
+const completionScoring = {
+  ')': 1,
+  ']': 2,
+  '}': 3,
+  '>': 4,
+}
+
+const matchingBrackets = {
+  '(': ')',
+  '[': ']',
+  '{': '}',
+  '<': '>',
+}
+
+const isOpeningBracket = (bracket) => ['{', '[', '(', '<'].includes(bracket)
+const isCorrectClosingBracket = (openingBracket, closingBracket) => {
+  if (!openingBracket) {
+    return false
+  } 
+  return matchingBrackets[openingBracket] === closingBracket
+}
+
+const getOpeningBrackets = (line) => {
+  const brackets = line.split('')
+  const openingBrackets = []
+  const hasError = brackets.some((bracket) => {
+    if (isOpeningBracket(bracket)) {
+      openingBrackets.push(bracket)
+    } else if (!isCorrectClosingBracket(openingBrackets.pop(), bracket)) {
+      return true
+    }
+  })
+
+  return [hasError, openingBrackets]
+}
+
+const getCompletionScore = (openingBrackets) => {
+  return openingBrackets
+    .reverse()
+    .reduce(
+      (score, bracket) =>
+        score * 5 + completionScoring[matchingBrackets[bracket]],
+      0
+    )
+}
+
+const solve = (data) => {
+  const lines = data.split('\n')
+  const completedLineScores = lines.reduce((completed, line) => {
+    const [hasError, openingBrackets] = getOpeningBrackets(line)
+    if (hasError) {
+      return completed
+    } else {
+      return [...completed, getCompletionScore(openingBrackets)]
+    }
+  }, [])
+  completedLineScores.sort((a, b) => a - b)
+  const middleIndex = Math.floor(completedLineScores.length / 2)
+  return completedLineScores[middleIndex]
+}
+
+(async () => {
+const data = await readInput('./inputs.txt')
+
+console.log(solve(data))
+})()
